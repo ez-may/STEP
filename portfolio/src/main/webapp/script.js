1141ede0b4f72fd1d211c212a7e829628ab82c8e
Mathew Suarez <masulo99@gmail.com>
2020-06-02 20:37:43 +0000

    Alerts the user with a 'Hello Mathew' on load. Completes step 2 of the week 3 tutorial.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,2 +1,8 @@
 //TODO:(matwsuaz) : Update with logic to allow the implementation of a rotating image gallery with buttons, changing captions depending on what
 // image is being displayed, and a navigation bar which allows the user to navigate to different scroll positions.
+
+async function greeting() {
+    const response = await fetch("/data");
+    const msg = await  response.text();
+    alert(msg);
+}
